49e03cd02bde0b52fb3a7f214906b4724a1de4b0
TheZoq2 <frans.skarman@gmail.com>
2018-11-28 12:40:33 +0100

    Split spi module into several functions

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 sim:
 	stack exec -- clash --verilog spi.hs
 	cp verilog/Counter/counter/counter.v counter.v
-	cp verilog/Spi/SPIReader/SPIReader.v spi.v
-	iverilog -o spi_tb.out -D VCD_OUTPUT=spi_tb.vcd spi.v spi_test.v
+	cp verilog/Spi/SPIReader/* .
+	iverilog -o spi_tb.out -D VCD_OUTPUT=spi_tb.vcd *.v
 	vvp spi_tb.out
 
 
--- spi.hs
+++ spi.hs
@@ -5,7 +5,7 @@ import Clash.Explicit.Testbench
 
 
 
-data State
+data Step
     = Start
     | ClkRising
     | ClkUp
@@ -13,37 +13,76 @@ data State
     deriving Show
 
 
+data State = State
+    { step :: Step
+    , d :: Signed 8
+    }
 
-spiT (state, d) (clk, input) =
-    case state of
+
+initialState :: State
+initialState = State
+    { step = Start
+    , d = 0
+    }
+
+
+updateStep :: Step -> State -> State
+updateStep newStep State {d=d} =
+    State newStep d
+
+
+
+nextStep :: State -> (Bit, Bit) -> Step
+nextStep State {step=step} (clk, input) =
+    case step of
         Start ->
             case clk of
-                1 -> ((ClkRising, d), (d, low))
-                _ -> ((Start, d), (d, low))
+                1 -> ClkRising
+                _ -> Start
         ClkDown ->
             case clk of
-                1 -> ((ClkRising, d), (d, low))
-                _ -> ((Start, d), (d, low))
+                1 -> ClkRising
+                _ -> Start
         ClkRising ->
-            let
-                toAdd = case input of
-                    1 -> 1
-                    0 -> 0
-
-                newD = (shift d 1) + toAdd
-            in
-                ((ClkUp, newD), (newD, low))
+                ClkUp
         ClkUp ->
             case clk of
-                1 -> ((ClkDown, d), (d, low))
-                _ -> ((ClkUp, d), (d, low))
+                1 -> ClkDown
+                _ -> ClkUp
+
+
+newData :: State -> (Bit, Bit) -> Signed 8
+newData State {step=step, d=d} (clk, input) =
+    case step of
+        ClkRising ->
+            shift d 1 + fromInteger (toInteger input)
+        _ ->
+            d
+
+
+
+output :: State -> (Bit, Bit) -> (Signed 8, Bit)
+output State {step=step, d=d} (clk, input) =
+    case step of
+        ClkRising ->
+            (d, high)
+        _ ->
+            (d, low)
+
+
+
+spiT :: State -> (Bit, Bit) -> (State, (Signed 8, Bit))
+spiT state input =
+    ( State {step = nextStep state input, d = newData state input}
+    , output state input
+    )
 
 
 -- maskedCounter :: HiddenClockReset domain gated synchronous =>
 --     Signal domain a -> Signal domain a
 -- spi :: Signal System (Bit, Bit) -> Signal System a
 spi =
-    mealy spiT (Start, 0)
+    mealy spiT initialState
 
 
 {-# ANN topEntity
