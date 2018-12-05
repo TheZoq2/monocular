04a7168023688c9c4ce0dfee2119d4d6254078a8
TheZoq2 <frans.skarman@gmail.com>
2018-12-05 23:30:35 +0100

    Add failing test for time updates

--- test/main_tb.v
+++ test/main_tb.v
@@ -7,6 +7,10 @@ module main_tb();
     reg [7:0] pin_values;
     wire miso;
 
+    reg [31:0] current_time;
+    reg [31:0] tx_start_time;
+    integer i;
+
     initial begin
         $dumpfile(`VCD_OUTPUT);
         $dumpvars(0, main_tb);
@@ -14,10 +18,19 @@ module main_tb();
         mosi = 0;
         spi_clk = 0;
         pin_values = 0;
+        current_time = 0;
         #1;
         forever begin
             #1 clk = ~clk;
+
         end
+    end
+
+    always @(posedge clk) begin
+        if (rst == 1)
+            current_time = 0;
+        else
+            current_time = current_time + clk;
     end
 
     initial begin
@@ -47,9 +60,42 @@ module main_tb();
         `ASSERT_EQ(miso, 0);
         spi_clk = 1; #4 spi_clk = 0; #4;
 
+        // Set some new data
+        pin_values = 'b0010_1101;
+        tx_start_time = current_time;
+
+
         // Check clock data
         repeat (32) begin
             `ASSERT_EQ(miso, 0);
+            spi_clk = 1;
+            #4;
+            spi_clk = 0;
+            #4;
+        end
+
+        // Ensure that the new data is being transmitted
+        #4;
+        `ASSERT_EQ(miso, 0);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 0);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 1);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 0);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 1);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 1);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 0);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+        `ASSERT_EQ(miso, 1);
+        spi_clk = 1; #4 spi_clk = 0; #4;
+
+        // Check clock data
+        for (i = 0; i < 32; i = i + 1) begin
+            `ASSERT_EQ(miso, tx_start_time[i]);
             spi_clk = 1;
             #4;
             spi_clk = 0;
