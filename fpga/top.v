65230fc4feff49613a4cc98c6804b60d3c7504d1
TheZoq2 <frans.skarman@gmail.com>
2018-12-23 09:56:10 +0100

    Change pinout for new pi hat

--- fpga/top.v
+++ fpga/top.v
@@ -3,9 +3,9 @@ module top (
     input CLK,    // 16MHz clock
     output LED,   // User/boot LED next to power LED
     output USBPU,  // USB pull-up resistor
-    output PIN_3,
-    input PIN_1,
-    input PIN_2,
+    output PIN_19,
+    input PIN_20,
+    input PIN_18,
 
     // Reset pin
     input PIN_13,
@@ -13,55 +13,35 @@ module top (
     // Debug pin
     output PIN_4,
 
+    input PIN_1,
+    input PIN_2,
+    input PIN_3,
+    input PIN_4,
     input PIN_5,
     input PIN_6,
     input PIN_7,
     input PIN_8,
-    input PIN_9,
-    input PIN_10,
-    input PIN_11,
-    input PIN_12,
-
-    output PIN_14,
-    output PIN_15,
-    output PIN_16,
-    output PIN_17,
-    output PIN_18,
-    output PIN_19,
-    output PIN_20,
-    output PIN_21,
 );
     wire [7:0] spi_read_byte;
     wire new_spi_byte;
 
-    wire spi_clk = PIN_1;
-    wire mosi = PIN_2;
-    wire miso = PIN_3;
-    wire spi_debug = PIN_4;
-    wire reset = PIN_13;
+    wire spi_clk = PIN_18;
+    wire mosi = PIN_20;
+    wire miso = PIN_19;
+    // wire reset = PIN_13;
+    wire reset = 0;
 
 
-    // SPIReader reader
-    //     ( .clk(CLK)
-    //     , .rst(reset)
-    //     , .spi_clk(spi_clk_buffered)
-    //     , .mosi(mosi)
-    //     , .miso(miso)
-    //     , .data(spi_read_byte)
-    //     , .received(new_spi_byte)
-    //     , .to_output('b10000001)
-    //     );
+    wire [7:0] pin_values;
 
-    wire [7:0] pin_values =
-        { PIN_5
-        , PIN_6
-        , PIN_7
-        , PIN_8
-        , PIN_9
-        , PIN_10
-        , PIN_11
-        , PIN_12
-        };
+    assign pin_values[0] = PIN_1;
+    assign pin_values[1] = PIN_2;
+    assign pin_values[2] = PIN_3;
+    assign pin_values[3] = PIN_4;
+    assign pin_values[4] = PIN_5;
+    assign pin_values[5] = PIN_6;
+    assign pin_values[6] = PIN_7;
+    assign pin_values[7] = PIN_8;
 
 
     main_module main
@@ -111,6 +91,7 @@ module top (
     ////////////////////////////////////////////////////////////////////////////////
     //                          Debug output
     ////////////////////////////////////////////////////////////////////////////////
+    /*
     assign
         { PIN_14
         , PIN_15
@@ -121,6 +102,7 @@ module top (
         , PIN_20
         , PIN_21
         } = pin_values;
+    */
 endmodule
 
 
